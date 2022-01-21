fd7acb06cbed6db6c3d27990feca320570b5d0ff
Jean Soto <jpsotochirinos@gmail.com>
2022-01-21 15:57:02 -0500

    jn

---
+++ frcnn.py
@@ -0,0 +1,9 @@
+# importing required libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+#matplotlib inline
+from matplotlib import patches
+
+# read the csv file using read_csv function of pandas
+train = pd.read_csv(‘train.csv’)
+train.head()
